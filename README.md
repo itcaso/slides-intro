8f80fd397831fd4478af0f745064b39eb9835b1e
itcaso <italoc@gmail.com>
2016-12-05 02:14:57 -0600

    Primer release
    * Agregado de los slides
    * Agregado de imagenes de prueba
    * Agregado del logo
    * Agregado de efecto en el logo

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# slides-intro
+# slides-intro 1.0.0
+
+## Plugins usados
+
+* jQuery v1.11.3  (practicamente, para que funcione todo)
+* Bootstrap (solo para acomodar un poco el estilo)
+* carouFredSel-jQuery (para los slides)
+* Modernizr 2.6.2 (para el efecto del loader)
+* CSS3 Animate it (para el efecto del logotipo)
--- js/main.js
+++ js/main.js
@@ -2,8 +2,45 @@
 (function($) {
     'use strict';
 
+    $.redirectURL = redirectURL; 
+
     setTimeout(function(){
 		$('body').addClass('loaded');
-	}, 3000);
+	}, 3000); //Tiempo que se muestra el loader inicial
+
+	 $('#slide01').carouFredSel({ // Slider superior
+	 	infinite: true,
+	 	items: 10,
+	 	responsive: false,
+	 	align: "center",
+	 	scroll: {
+			items: 1,
+			duration: 2500,
+			timeoutDuration: 0,
+			easing: 'linear'
+		}
+	 });
+
+	 $('#slide02').carouFredSel({ // Slider inferior
+	 	infinite: true,
+	 	items: 10,
+	 	direction: "right",
+	 	responsive: false,
+	 	align: "center",
+	 	scroll: {
+			items: 1,
+			duration: 2500,
+			timeoutDuration: 0,
+			easing: 'linear'
+		}
+	 });
+
+	 function redirectURL(){
+	 	var delay = 10000; //Tiempo en milisegundos
+		setTimeout(function(){ window.location = 'http://yahoo.com'; }, delay); //Cambiar la URL por la pagina del sitio
+	 }
+
+	 // Redireccionamiento desactivado en index.html, quitar comentarios para activarlo
+	 //redirectURL();
 
 })(jQuery);
